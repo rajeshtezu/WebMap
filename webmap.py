5e9be88df9e610ed3aed374aa57aaa365c096678
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-09-07 21:37:51 +0530

    marker fill_color working

--- webmap.py
+++ webmap.py
@@ -1,4 +1,4 @@
-# /usr/bin/python3
+#! /usr/bin/python3
 
 import folium
 import pandas
@@ -20,8 +20,7 @@ map = folium.Map(location=[38.36, -99.03], zoom_start=6, tiles="Mapbox Bright")
 
 fgv = folium.FeatureGroup(name="Volcanoes")
 for lt, ln, el in zip(lat, lon, elev):
-    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=5, popup=str(el)+" m", 
-    fill_color=color_producer(el), color='grey', fill_opacity=0.7))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=5, color='grey', popup=str(el)+" m", fill_color=color_producer(el), fill=True, fill_opacity=0.7))
 
 
 fgp = folium.FeatureGroup(name="Populations")
@@ -30,11 +29,10 @@ fgp = folium.FeatureGroup(name="Populations")
 with open("world.json", "r", encoding="utf-8-sig") as f:
     fgp.add_child(folium.GeoJson(data=f.read(),
     style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
-    else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red' }))    
+    else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red' }))
 
 map.add_child(fgv)
 map.add_child(fgp)
 map.add_child(folium.LayerControl())
 
 map.save("Map.html")
-
